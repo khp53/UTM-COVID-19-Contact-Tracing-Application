6a3e0e27803365b0a61e1eda935a6be2e3a8dd9e
khp53 <khp4121@gmail.com>
2021-04-09 18:06:55 +0800

    version 5.1

--- lib/Application/registerForm.dart
+++ lib/Application/registerForm.dart
@@ -31,44 +31,50 @@ class _RegisterMobileNumberState extends State<RegisterMobileNumber> {
   AuthCredential authCreds;
 
   UserDA _userDA = UserDA();
-  Users _users = Users();
+  //Users _users = Users();
 
   Users _userFromFirebaseUser(User user) {
     return user != null ? Users(uid: user.uid) : null;
   }
 
   Future<void> verifyPhone(phoneNo) async {
-    //if (formKey.currentState.validate()) {
-    final PhoneVerificationCompleted verified = (AuthCredential authResult) {
-      _userDA.signIn(authResult);
-    };
-
-    final PhoneVerificationFailed verificationfailed =
-        (FirebaseAuthException authException) {
+    if (formKey.currentState.validate()) {
       setState(() {
-        print('Error message:' + authException.message);
+        isLoading = true;
       });
-    };
+      final PhoneVerificationCompleted verified = (AuthCredential authResult) {
+        _userDA.signIn(authResult);
+      };
 
-    final PhoneCodeSent smsSent = (String verId, [int forceResend]) {
-      this.verificationId = verId;
+      final PhoneVerificationFailed verificationfailed =
+          (FirebaseAuthException authException) {
+        setState(() {
+          print('Error message:' + authException.message);
+        });
+      };
+
+      final PhoneCodeSent smsSent = (String verId, [int forceResend]) {
+        this.verificationId = verId;
+        setState(() {
+          this.codeSent = true;
+        });
+      };
+
+      final PhoneCodeAutoRetrievalTimeout autoTimeout = (String verId) {
+        this.verificationId = verId;
+      };
+
+      await FirebaseAuth.instance.verifyPhoneNumber(
+          phoneNumber: phoneNo,
+          timeout: const Duration(seconds: 120),
+          verificationCompleted: verified,
+          verificationFailed: verificationfailed,
+          codeSent: smsSent,
+          codeAutoRetrievalTimeout: autoTimeout);
       setState(() {
-        this.codeSent = true;
+        isLoading = true;
       });
-    };
-
-    final PhoneCodeAutoRetrievalTimeout autoTimeout = (String verId) {
-      this.verificationId = verId;
-    };
-
-    await FirebaseAuth.instance.verifyPhoneNumber(
-        phoneNumber: phoneNo,
-        timeout: const Duration(seconds: 120),
-        verificationCompleted: verified,
-        verificationFailed: verificationfailed,
-        codeSent: smsSent,
-        codeAutoRetrievalTimeout: autoTimeout);
-    //}
+    }
   }
 
   signInUser() async {
@@ -101,7 +107,7 @@ class _RegisterMobileNumberState extends State<RegisterMobileNumber> {
             return Navigator.pushAndRemoveUntil(
               context,
               MaterialPageRoute(
-                builder: (BuildContext context) => HealthStatusForm(),
+                builder: (BuildContext context) => RegisterUserData(),
               ),
               (route) => false,
             );
@@ -109,7 +115,7 @@ class _RegisterMobileNumberState extends State<RegisterMobileNumber> {
             return Navigator.pushAndRemoveUntil(
               context,
               MaterialPageRoute(
-                builder: (BuildContext context) => Homepage(),
+                builder: (BuildContext context) => RegisterUserData(),
               ),
               (route) => false,
             );
@@ -187,7 +193,6 @@ class _RegisterMobileNumberState extends State<RegisterMobileNumber> {
                     Container(
                       padding: EdgeInsets.only(left: 5, right: 5),
                       child: TextFormField(
-                        enabled: _enabled,
                         controller: _phoneNumberController,
                         keyboardType: TextInputType.phone,
                         validator: (value) {
@@ -203,12 +208,6 @@ class _RegisterMobileNumberState extends State<RegisterMobileNumber> {
                                 color: Colors.white70,
                               ),
                             ),
-                            disabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(5),
-                              borderSide: BorderSide(
-                                color: Colors.grey,
-                              ),
-                            ),
                             focusedBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(5),
                               borderSide: BorderSide(
@@ -220,33 +219,14 @@ class _RegisterMobileNumberState extends State<RegisterMobileNumber> {
                       ),
                     ),
                     SizedBox(
-                      height: MediaQuery.of(context).size.height / 55,
-                    ),
-                    Container(
-                      child: Text(_emptyError),
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height / 65,
+                      height: MediaQuery.of(context).size.height / 40,
                     ),
                     Container(
                       padding: EdgeInsets.only(right: 2),
                       child: !isLoading
                           ? TextButton(
                               onPressed: () {
-                                if (_phoneNumberController.text.isNotEmpty) {
-                                  setState(() {
-                                    isLoading = true;
-                                    _enabled = !_enabled;
-                                  });
-
-                                  verifyPhone(_phoneNumberController.text);
-                                } else {
-                                  setState(() {
-                                    isLoading = false;
-                                  });
-                                  _emptyError =
-                                      'Please Enter a Valid Phone Number';
-                                }
+                                verifyPhone(_phoneNumberController.text);
                               },
                               child: Container(
                                 height: 50,
@@ -259,7 +239,9 @@ class _RegisterMobileNumberState extends State<RegisterMobileNumber> {
                                             fontSize: 15))),
                               ),
                             )
-                          : CircularProgressIndicator(),
+                          : Container(
+                              padding: EdgeInsets.only(top: 5),
+                              child: CircularProgressIndicator()),
                     ),
                   ],
                 ),
@@ -452,6 +434,283 @@ class _RegisterMobileNumberState extends State<RegisterMobileNumber> {
                   ),
                 ),
               ),
+      ),
+    );
+  }
+}
+
+//Complete registration after mobile number has been authenticated and user id has been created
+
+class RegisterUserData extends StatefulWidget {
+  @override
+  _RegisterUserDataState createState() => _RegisterUserDataState();
+}
+
+class _RegisterUserDataState extends State<RegisterUserData> {
+  final formKey = new GlobalKey<FormState>();
+  bool isLoading = false;
+
+  // Define the text controllers to capture user data
+  final TextEditingController _userIDController = TextEditingController();
+  final TextEditingController _icNoController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _currentAddressController =
+      TextEditingController();
+  final TextEditingController _postCodeController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height / 12, 20, 20),
+            child: Center(
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 74,
+                      height: 74,
+                      child: Image(
+                        image: AssetImage('assets/img/logo.png'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Register',
+                      style: Theme.of(context).textTheme.headline1,
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 15,
+                    ),
+                    Container(
+                      child: Text(
+                        'Please complete your registration by providing us your information.\nThis information will only be accesed by an UTM admin\nIf you are tested positive with COVID-19.',
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 45,
+                    ),
+                    //userID or Matric number Form Field.
+                    Container(
+                      padding: EdgeInsets.only(left: 5, right: 5),
+                      child: TextFormField(
+                        controller: _userIDController,
+                        validator: (value) {
+                          return value.isEmpty
+                              ? "Please Enter a valid matric number!"
+                              : null;
+                        },
+                        style: Theme.of(context).textTheme.bodyText1,
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                color: Colors.white70,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            hintText: "Matric Number (i.e: A20XX4123)",
+                            hintStyle: Theme.of(context).textTheme.bodyText2),
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 65,
+                    ),
+                    //IC number or Passport Number Form Field.
+                    Container(
+                      padding: EdgeInsets.only(left: 5, right: 5),
+                      child: TextFormField(
+                        controller: _userIDController,
+                        validator: (value) {
+                          return value.isEmpty
+                              ? "Please Enter a valid IC or Passport No.!"
+                              : null;
+                        },
+                        style: Theme.of(context).textTheme.bodyText1,
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                color: Colors.white70,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            hintText: "IC/Passport Number",
+                            hintStyle: Theme.of(context).textTheme.bodyText2),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 65,
+                    ),
+                    //Full name Form Field.
+                    Container(
+                      padding: EdgeInsets.only(left: 5, right: 5),
+                      child: TextFormField(
+                        controller: _nameController,
+                        validator: (value) {
+                          return value.isEmpty
+                              ? "Please Enter your name!"
+                              : null;
+                        },
+                        style: Theme.of(context).textTheme.bodyText1,
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                color: Colors.white70,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            hintText: "Full Name",
+                            hintStyle: Theme.of(context).textTheme.bodyText2),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 65,
+                    ),
+                    //Email Form Field.
+                    Container(
+                      padding: EdgeInsets.only(left: 5, right: 5),
+                      child: TextFormField(
+                        controller: _emailController,
+                        validator: (value) {
+                          return RegExp(
+                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                  .hasMatch(value)
+                              ? null
+                              : "Provide a valid email";
+                        },
+                        style: Theme.of(context).textTheme.bodyText1,
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                color: Colors.white70,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            hintText: "Email Address",
+                            hintStyle: Theme.of(context).textTheme.bodyText2),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 65,
+                    ),
+                    //Current Address Form Field.
+                    Container(
+                      padding: EdgeInsets.only(left: 5, right: 5),
+                      child: TextFormField(
+                        controller: _currentAddressController,
+                        validator: (value) {
+                          return value.isEmpty
+                              ? "Please Enter your full address!"
+                              : null;
+                        },
+                        style: Theme.of(context).textTheme.bodyText1,
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                color: Colors.white70,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            hintText: "Full Address",
+                            hintStyle: Theme.of(context).textTheme.bodyText2),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 65,
+                    ),
+                    //Postcode Form Field.
+                    Container(
+                      padding: EdgeInsets.only(left: 5, right: 5),
+                      child: TextFormField(
+                        controller: _postCodeController,
+                        validator: (value) {
+                          return value.isEmpty
+                              ? "Please Enter your area postcode!"
+                              : null;
+                        },
+                        style: Theme.of(context).textTheme.bodyText1,
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                color: Colors.white70,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            hintText: "Postcode",
+                            hintStyle: Theme.of(context).textTheme.bodyText2),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 75,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        bottomNavigationBar: Container(
+          padding: EdgeInsets.only(right: 2),
+          child: !isLoading
+              ? TextButton(
+                  onPressed: () {},
+                  child: Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: mainButton(),
+                    child: Center(
+                        child: Text('Submit',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 15))),
+                  ),
+                )
+              : CircularProgressIndicator(),
+        ),
       ),
     );
   }
