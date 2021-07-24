ac2c92e5bbece34ab8eab37ebfab7547154cc445
KHP53 <khp4121@gmail.com>
2021-07-25 02:41:40 +0600

    added contact exposure alert

---
+++ backend/functions/index.js
@@ -0,0 +1,36 @@
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().functions);
+
+const db = admin.firestore();
+const fcm = admin.messaging();
+
+exports.sendExposureNotification = functions.firestore.document('HealthStatus/{userId}').onUpdate(async snapshot => {
+    var tokens = [];
+    //const user = snapshot.data();
+    const oldSnap = snapshot.before.data();
+    const newSnap = snapshot.after.data();
+
+    if (oldSnap.covidStatus == false && newSnap.covidStatus == true){
+
+        const querySnapshot = await db.collection('TraceContacts').doc(newSnap.documentId).collection('contactedWith').get();
+        for (var token of querySnapshot.docs) {
+            tokens.push(token.data().deviceToken);
+        }
+
+        const payload = {
+            notification: {
+            title: 'Contact Exposure Alert!',
+            body: 'One of your contacts has been tested positive for COVID-19!',
+            click_action: 'FLUTTER_NOTIFICATION_CLICK'
+            }
+        };
+    
+        try {
+            const response = await fcm.sendToDevice(tokens, payload);
+            console.log('Notification sent successfully');
+        } catch (err) {
+            console.log(err);
+        }
+    }
+});
